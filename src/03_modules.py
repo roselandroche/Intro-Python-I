d97ec2bbac8ba919f58db31f49cca4144b64f3e6
roselandroche <r.landroche@gmail.com>
2020-06-04 19:46:57 -0400

    Edit to 03

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,8 +10,8 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-for x in sys.argv:
-    print('Line:', x)
+for arg in sys.argv:
+    print('Line:', arg)
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
